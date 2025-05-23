e82ef608525deee0c7d7f3f8e5e56ed727b32f80
Nieraldo da Silva Lima <12436720@comlurb.rio.rj.gov.br>
2025-05-23 14:28:12 -0300

    gluestack e nativewind

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
       ["react-native-worklets-core/plugin"],
-      "react-native-reanimated/plugin",
+      // "react-native-reanimated/plugin",
     ],
   };
 };
